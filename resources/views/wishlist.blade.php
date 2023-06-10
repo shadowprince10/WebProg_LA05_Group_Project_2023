0eb7fe4aa32c66fd7ca7e955d5ce5f60243eba38
shadowprince10 <belinda.jiahong@gmail.com>
2023-06-10 22:36:10 +0700

    fifteenth commit

--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -9,6 +9,7 @@
 <body>
     @extends('layout_format.header')
 
+    @section('container')
     @if ($wishlist -> isEmpty())
         <a href = "{{ route('homepage') }}" class = "btn btn-primary">+ Create New Wishlist</a>
     {{-- href = "{{ route('home') }} to redirect user to home page to add products to the wishlist once the user clicked on the button "+ Create New Wishlist" --}}
@@ -16,11 +17,13 @@
         <ul>
             @foreach($wishlist as $wl)
                 <li>
+                    <img src = "{{ asset('assets/products/' . $wl -> product -> image) }}" class = "card-img-top img-fluid" style = "height: 400px;">
                     <h3><b>{{ $wl -> product -> name }}</b></h3>
                     <p>{{ $wl -> product -> price }}</p>
                 </li>
             @endforeach
         </ul>
+    @endsection
 
     @extends('layout_format.footer')
 </body>
